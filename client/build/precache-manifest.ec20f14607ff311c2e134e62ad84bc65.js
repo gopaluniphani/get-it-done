13f5d2bf092294175616bf227fb7c172c3a536fb
Phani Gopaluni <gopaluniphani@gmail.com>
2020-07-27 10:09:43 +0530

    Added build files for client

---
+++ client/build/precache-manifest.ec20f14607ff311c2e134e62ad84bc65.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d58efe5d00178abbd57cfe0a4e08931",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c6b61f84479d24b4d1f",
+    "url": "/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "8c6b61f84479d24b4d1f",
+    "url": "/static/js/2.34d2ece9.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/static/js/2.34d2ece9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1eee0830cc63eeb5e8d",
+    "url": "/static/js/main.f3b05497.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "62fd37ef3203a999f7b9cfdaa4911e86",
+    "url": "/static/media/background.62fd37ef.png"
+  },
+  {
+    "revision": "a5cd770e426dff43aef05a3c33d1293a",
+    "url": "/static/media/lawyer.a5cd770e.jpg"
+  },
+  {
+    "revision": "b5adad4ecf0a9fa0d5ae053b76f8022e",
+    "url": "/static/media/stethoscope.b5adad4e.jpg"
+  }
+]);
